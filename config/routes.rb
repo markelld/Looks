340e7b89f3d161ce2fadb555c5c1e4f157ad4dae
“markelld” <markelldehaney@gmail.com>
2021-12-07 13:32:57 -0600

    messaging system added for conversations between users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+  get 'conversations/index'
   resources :orders
   # get 'home/index' 
   root "home#index"
   resources :stylists
-  devise_for :users
+  devise_for :users 
+
+  resources :conversations,only: [:index, :create] do 
+    resources :messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
